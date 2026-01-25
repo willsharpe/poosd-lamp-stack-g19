155683ecfb4b381397062c9de5b1d3efab000fa7
OzBoz <ozairboss2005@gmail.com>
2026-01-25 14:00:56 -0500

    Test User GET

---
+++ LAMP Project/API/user_flow/login.php
@@ -0,0 +1,57 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+	$email = "";
+	$conn = new mysqli("localhost", "lamp_G19", "WeLoveCOP4331", "ContactManager"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT id,firstname,lastname FROM Users WHERE email =? AND password =?");
+		$stmt->bind_param("ss", $inData["email"], $inData["password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc()  )
+		{
+			returnWithInfo( $row['firstname'], $row['lastname'], $row['id'], $row['email'] );
+		}
+		else
+		{
+			returnWithError("No User Found");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstname":"' . $firstName . '","lastname":"' . $lastName . '","email":"' . $email . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
