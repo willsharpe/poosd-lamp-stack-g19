7a9535b3730de323dc37564228af8c475f75c489
OzBoz <ozairboss2005@gmail.com>
2026-01-29 12:47:16 -0500

    Readallcontacts first draft

---
+++ LAMP Project/API/contacts_flow/readallcontacts.php
@@ -0,0 +1,48 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$conn = new mysqli("localhost", "lamp_G19", "WeLoveCOP4331", "ContactManager"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError($conn->connect_error, 500);
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT firstname, lastname, phone FROM Users WHERE parent_id = ?");
+		$stmt->bind_param("i", $inData["parent_id"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+        $data = array();
+
+		if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()){
+                $data[] = $row;
+            }
+        }
+
+        sendResultInfoAsJson(json_encode($data));
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err , $code)
+	{
+		http_response_code($code);
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
